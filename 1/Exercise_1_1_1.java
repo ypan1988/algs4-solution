a01e3939c1b67ac3398bdf2011202c7e7932f779
Yi Pan <ypan1988@gmail.com>
2021-05-30 12:52:06 +0100

    add 1.1.2

--- 1/Exercise_1_1_1.java
+++ 1/Exercise_1_1_1.java
@@ -1,10 +1,10 @@
 public class Exercise_1_1_1 {
     public static void main(String[] args) {
-	int resultA = (0 + 15) / 2;
-	double resultB = 2.0e-6 * 100000000.1;
-	boolean resultC = true && false || true && true;	
-	System.out.println("a) " + resultA);
-	System.out.println("b) " + resultB);
-	System.out.println("c) " + resultC);
+        int resultA = (0 + 15) / 2;
+        double resultB = 2.0e-6 * 100000000.1;
+        boolean resultC = true && false || true && true;
+        System.out.println("a) " + resultA);
+        System.out.println("b) " + resultB);
+        System.out.println("c) " + resultC);
     }
 }
---
+++ 1/Exercise_1_1_2.java
@@ -0,0 +1,12 @@
+public class Exercise_1_1_2 {
+    public static void main(String[] args) {
+        double resultA = (1 + 2.236) / 2;
+        double resultB = 1 + 2 + 3 + 4.0;
+        boolean resultC = 4.1 >= 4;
+        String resultD = 1 + 2 + "3";
+        System.out.println("a) " + resultA);
+        System.out.println("b) " + resultB);
+        System.out.println("c) " + resultC);
+        System.out.println("d) " + resultD);
+    }
+}
